a0f564e482648bfb3afc7a39daf97b5a31a7811d
Catherine <catblecon@gmail.com>
2020-04-04 09:42:08 +0100

    compenent user ok

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <UsersListFixed />
     </div>
     
-    </div>
   );
 }
 
--- src/components/User.js
+++ src/components/User.js
@@ -1,31 +1,32 @@
 import React from "react";
 
-const Geo = ({ lat, lng}) => (
+const Geo = ({ geogr}) => (
   <div className="geo">
-      <p>{lat}</p>
-      <p>{lng}</p>
+      <p>{geogr.lat}</p>
+      <p>{geogr.lng}</p>
   </div>
 );
 
-const Address = ({ street, suite,city, zipcode}) => (
+const Address = ({adr}) => (
   <div className="address">
-      <p>{street}</p>
-      <p>{suite}</p>
-      <p>{city}</p>
-      <p>{zipcode}</p>
-      <p>{suite}</p>
-      < Geo />
+      <p>{adr.street}</p>
+      <p>{adr.suite}</p>
+      <p>{adr.city}</p>
+      <p>{adr.zipcode}</p>
+      <p>{adr.suite}</p>
+      <Geo  geogr={adr.geo} />  
+      
   </div>
 );
 
-const User = ({ id, name, username, email}) => (
+const User = ({ id, name, username, email, address}) => (
   <div className="user">
-  <p>{id}</p>
-  <p>{name}</p>
-  <p>{username}</p>
-  <p>{email}</p>
-  < Address />
-</div>
+      <p>{id}</p>
+      <p>{name}</p>
+      <p>{username}</p>
+      <p>{email}</p>
+      <Address  adr={address} />   
+  </div>
 );
 
 export default User;
